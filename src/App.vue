3df18d8a332c08bf60ab96d2d6b94d13538846b9
白石祐大 <shiraishi_yuma@uniquevision.co.jp>
2023-02-11 19:55:51 +0900

    fix: 疎通確認のためにフロントから API を呼び出してみる

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+
+const prefectures = ref<any>()
+const nature = ref<any>()
+
+onMounted(async () => {
+  prefectures.value = await (await fetch('/api/prefectures')).json()
+  nature.value = await (await fetch('/api/population/nature?cityCode=11362&ageTo=-&prefCode=11&ageFrom=30')).json()
+})
 </script>
 
 <template>
   <HelloWorld msg="hello" />
+  {{ prefectures }}
+  {{ nature }}
 </template>
