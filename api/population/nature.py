3edbe674b18f5c143819f6fb5a8e54c10d526d6d
白石祐大 <shiraishi_yuma@uniquevision.co.jp>
2023-02-11 19:47:50 +0900

    add: 人口自然増減数をクエリパラメータ付きで取得するエンドポイント

---
+++ api/population/nature.py
@@ -0,0 +1,23 @@
+from http.server import BaseHTTPRequestHandler
+from urllib.parse import parse_qs, urlparse
+import urllib.request
+import os
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        url = 'https://opendata.resas-portal.go.jp/api/v1/population/nature' + '?' + parsed_path.query
+        print(parsed_path)
+        print(url)
+
+        req = urllib.request.Request(url)
+        req.add_header('X-API-KEY',os.environ.get('RESAS_API_KEY'))
+        with urllib.request.urlopen(req) as res:
+            body = res.read()
+
+        self.send_response(200)
+        self.send_header('Content-type','application/json; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(body)
+        return
