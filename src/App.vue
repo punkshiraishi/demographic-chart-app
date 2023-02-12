d9cc2a86caf5932b48c1f9c016cf724fda5fb4e6
白石祐大 <shiraishi_yuma@uniquevision.co.jp>
2023-02-12 18:01:33 +0900

    wip: 取得した結果をグラフに表示できるようにした

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
+import { range } from 'lodash'
 import ArrayCheckbox from './components/ArrayCheckbox.vue'
+import LineChart from './components/LineChart.vue'
 import { useApi } from './hooks/useApi'
+
 import type { PopulationNature } from './types/populationNature'
 import type { Prefecture } from './types/prefecture'
 
 const prefectures = ref<Prefecture[]>([])
-const selectedPrefectures = ref<Prefecture[]>([])
+const selectedPrefectureCode = ref<number[]>([])
 
 const { getPrefectures, getPoplationNature } = useApi()
 
@@ -14,23 +17,42 @@ onMounted(async () => {
   prefectures.value = await getPrefectures()
 })
 
-const fechedPopulationNature = ref<PopulationNature[]>([])
+const fechedPopulationNatures = ref<{
+  prefecture: Prefecture
+  data: PopulationNature
+}[]>([])
 
 async function onCheck(prefecture: Prefecture) {
-  fechedPopulationNature.value.push(
-    await getPoplationNature(prefecture.prefCode),
-  )
+  fechedPopulationNatures.value.push({
+    prefecture,
+    data: await getPoplationNature(prefecture.prefCode),
+  })
 }
+
+const years = range(1985, 2021, 5)
+
+const datasets = computed(() => fechedPopulationNatures.value
+  .filter(population => !selectedPrefectureCode.value.includes(population.prefecture.prefCode))
+  .map(population => ({
+    name: population.prefecture.prefName,
+    data: population.data.bar.mandata
+      .map((man, index) => man.value + population.data.bar.womandata[index].value),
+  })))
 </script>
 
 <template>
   <ArrayCheckbox
     v-for="prefecture in prefectures"
     :key="prefecture.prefCode"
-    v-model="selectedPrefectures"
+    v-model="selectedPrefectureCode"
     :item="prefecture"
     :label="prefecture.prefName"
     @check="onCheck(prefecture)"
   />
-  {{ fechedPopulationNature }}
+  <LineChart
+    :labels="years"
+    :datasets="datasets"
+  />
+  {{ selectedPrefectureCode }}
+  {{ datasets }}
 </template>
