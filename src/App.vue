57c4d01021a6a827dade379690ac5afd0f652163
白石祐大 <shiraishi_yuma@uniquevision.co.jp>
2023-02-12 16:08:16 +0900

    wip: 取得した都道府県から動的にチェックボックスを作成する

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
+import ArrayCheckbox from './components/ArrayCheckbox.vue'
+import type { Prefecture, PrefecturesResponse } from './types/prefecture'
 
-const prefectures = ref<any>()
+const prefectures = ref<Prefecture[]>([])
+const selectedPrefectures = ref<Prefecture[]>([])
 const nature = ref<any>()
 
 onMounted(async () => {
-  prefectures.value = await (await fetch('/api/prefectures')).json()
+  prefectures.value = (await (await fetch('/api/prefectures')).json() as PrefecturesResponse).result
   nature.value = await (await fetch('/api/population/nature?cityCode=11362&ageTo=-&prefCode=11&ageFrom=30')).json()
 })
 </script>
 
 <template>
-  {{ prefectures }}
-  {{ nature }}
+  <ArrayCheckbox
+    v-for="prefecture in prefectures"
+    :key="prefecture.prefCode"
+    v-model="selectedPrefectures"
+    :item="prefecture"
+    :label="prefecture.prefName"
+  />
+  {{ selectedPrefectures }}
 </template>
