b8cd47e00fc7f34b2a889532da792d8c12f24928
白石祐大 <shiraishi_yuma@uniquevision.co.jp>
2023-02-12 16:02:26 +0900

    add: API のレスポンスの json から型を生成

---
+++ src/types/populationNature.ts
@@ -0,0 +1,4 @@
+import type populationNatureSample from './samples/population-nature-sample.json'
+
+export type PopulationNatureResponse = typeof populationNatureSample
+export type PopulationNature = typeof populationNatureSample['result']
---
+++ src/types/prefecture.ts
@@ -0,0 +1,4 @@
+import type prefecturesSample from './samples/prefectures-sample.json'
+
+export type PrefecturesResponse = typeof prefecturesSample
+export type Prefecture = typeof prefecturesSample['result'][number]
