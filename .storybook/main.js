606039ec1de3960631890ec42d960e8cc32ad1ae
白石祐大 <pharmacy.sy@gmail.com>
2023-02-17 00:15:03 +0900

    setup: storybook test runner
    カバレッジも測れるようにした

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const istanbul = require('vite-plugin-istanbul');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.ts"
@@ -5,7 +7,16 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-coverage",
+    {
+      name: "@storybook/addon-coverage",
+      options: {
+        istanbul: {
+          include: ["**/components/**"],
+        },
+      },
+    },
   ],
   "framework": "@storybook/vue3",
   "core": {
@@ -13,5 +24,13 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
+  },
+  async viteFinal(config) {
+    config.plugins.push(istanbul({
+      "extension": [
+        ".vue"
+      ]
+    }));
+    return config;
+  },
 }
